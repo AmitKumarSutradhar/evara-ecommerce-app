631f58ca9612315fc7ff9436ace6d03320322143
Amit Kumar Sutradhar <amit596450@gmail.com>
2023-10-24 20:15:26 +0600

    Website Frontend Part Mastering

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EvaraController;
+use App\Http\Controllers\DashboardController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/', [EvaraController::class,'index'])->name('home');
+Route::get('/product-category', [EvaraController::class ,'category'])->name('product-category');
+Route::get('/product-details',[EvaraController::class,'product'])->name('product-detail');
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', [DashboardController::class,'index'])->name('dashboard');
 });
